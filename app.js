0d1269f08fe7c131a92751daf6713da09ea8052c
syed-azhar <93089810+syed-azhar@users.noreply.github.com>
2023-09-13 19:54:02 -0700

    javascript-assignment05

---
+++ app.js
@@ -0,0 +1,131 @@
+// assignment 05 javascript sir ghous ahmed
+
+
+// 1. Write a program that takes a positive integer from user &
+// display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number
+
+// round off value
+
+// var number = 3.4562
+// var numbers = Math.round(number)
+// console.log(numbers)
+
+
+// floor values
+
+// var number = 3.4562
+// var numbers = Math.floor(number)
+// console.log(numbers)
+
+// ceil values 
+
+
+// var number = 3.4562
+// var numbers = Math.ceil(number)
+// console.log(numbers)
+
+// random values
+
+// var numbers = Math.random()
+// console.log(numbers)
+
+
+// 3. Write a program that displays the absolute value of a
+// number.
+// E.g. absolute value of -4 is 4 & absolute value of 5 is 5
+
+// var numbers = -4
+// var number= Math.abs(numbers)
+// console.log(number)
+
+
+// 4. Write a program that simulates a dice using random()
+// method of JS Math class. Display the value of dice in your
+// browser.:
+
+// var dice = Math.round((Math.random() * 10))
+// console.log("Random Dice Value", " " + dice)
+
+
+
+// 1. Write a program that displays current date and time in
+// your browser.
+
+
+
+// var date = new Date();
+// console.log(date)
+
+// 2. Write a program that alerts the current month in words.
+// For example December.
+
+
+// var month = new Date();
+// console.log(month.getMonth())
+
+
+// 14. Write a program to generate your K-Electric bill in your
+// browser. All the amounts should be rounded off to 2
+// decimal places. Display the following fields:
+// a. Customer Name
+// b. Current Month
+
+// DATE METHODS | JAVASCRIPT
+
+// Page 5 of 5
+
+// c. Number of units
+// d. Charges per unit
+// e. Net Amount Payable (within Due Date)
+// f. Late Payment Surcharge
+// g. Gross Amount Payable (after Due Date)
+// Where,
+
+// Net Amount Payable (within Due Date) = Number of units * Charges per unit
+// & Gross Amount Payable (after Due Date) = Net Amount + Late Payment Surcharge
+
+
+// var customerName = "azhar";
+// var currentMonth = "september";
+// var numberOfunit = 50;
+// var chargesPerunit = 50;
+
+
+// var netAmountpayable = numberOfunit * chargesPerunit;
+// var grossAmountPayable = netAmountpayable + 300
+
+// document.write("<h1>Eletricity Bill</h1>")
+
+// document.write("Customer name " , customerName  + "<br>",
+//    "Current month   ",  currentMonth + "<br>",
+//    "Numberofunit   " ,   numberOfunit +  "<br>",
+//     "Chargesperunit   " ,  chargesPerunit + "<br>",
+//    "net amount payable With due date   ",  netAmountpayable  + " <br>", 
+//     "After payable with late fees  " ,   grossAmountPayable) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
